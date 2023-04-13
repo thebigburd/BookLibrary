33143090db7c112ea43ebef00576e58a9faa54ba
TheBigBurd <55106453+thebigburd@users.noreply.github.com>
2023-04-13 18:42:59 +0100

    Unit testing for UserService Class

--- src/test/java/com/thebigburd/LibraryApplication/Service/UserServiceTest.java
+++ src/test/java/com/thebigburd/LibraryApplication/Service/UserServiceTest.java
@@ -1,29 +1,25 @@
 package com.thebigburd.LibraryApplication.Service;
 
 import com.thebigburd.LibraryApplication.Entity.User;
-import com.thebigburd.LibraryApplication.Repository.BookRepository;
-import com.thebigburd.LibraryApplication.Repository.BorrowRepository;
 import com.thebigburd.LibraryApplication.Repository.UserRepository;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.AdditionalAnswers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-class UserServiceTest {
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
@@ -31,44 +27,129 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-    private User user;
+    @Test
+    public void testGetUserReturnsUserWhenExists() {
+        // Setup
+        User user = new User(1L, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1));
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
-    @BeforeEach
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
+        // Act
+        User result = userService.getUser(1L);
 
-        user = new User("JohnDoe@example.com", "John", "Doe", LocalDate.of(2000, 01, 01));
-        user.setId(1L);
+        // Assert
+        assertEquals(user, result);
+    }
 
-        when(userRepository.save(any(User.class))).thenAnswer(AdditionalAnswers.returnsFirstArg()); // Return the same User object with ID assigned
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-        when(userRepository.existsById(1L)).thenReturn(true); // Simulate a user with ID 1 existing in repository.
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetUserThrowsExceptionInvalidId() {
+        // Setup
+        when(userRepository.existsById(1L)).thenReturn(false);
 
-        userRepository.save(user);
+        // Act
+        userService.getUser(1L);
+
+        // Assert exception thrown
     }
 
     @Test
-    void testGetUser() {
-        User returnedUser = userService.getUser(1L);
+    public void testGetUserList() {
+        // Setup
 
-        assertNotNull(returnedUser);
-        assertEquals(user.getId(), returnedUser.getId());
-        assertEquals(user.getName(), returnedUser.getName());
+        List<User> userList = Arrays.asList(
+                new User(1L, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1)),
+                new User(2L, "jane.doe@example.com", "Jane", "Doe", LocalDate.of(1995, 1, 1))
+        );
+        when(userRepository.findAll()).thenReturn(userList);
+
+        // Act
+        List<User> result = userService.getUserlist();
+
+        // Assert
+        assertEquals(userList, result);
     }
 
     @Test
-    void getUserlist() {
+    public void testSaveUser() {
+        // Setup
+        User user = new User(null, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1));
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.empty());
+
+        // Act
+        userService.saveUser(user);
+
+        // Assert
+        verify(userRepository).save(user);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testSaveUserWithExistingEmail() {
+        // Setup
+        User user = new User(null, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1));
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        // Act
+        userService.saveUser(user);
+
+        // Assert exception thrown
     }
 
     @Test
-    void saveUser() {
+    public void testDeleteUser() {
+
+        // Setup
+        when(userRepository.existsById(1L)).thenReturn(true);
+
+        // Act
+        userService.deleteUser(1L);
+
+        // Assert
+        verify(userRepository).deleteById(1L);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testDeleteNonExistingUser() {
+        // Setup
+        when(userRepository.existsById(1L)).thenReturn(false);
+
+
+        // Act
+        userService.deleteUser(1L);
+
+        // Assert exception thrown
     }
 
     @Test
-    void deleteUser() {
+    public void testUpdateUser() {
+
+        // SetUp
+        User user = new User(1L, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1));
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userRepository.findUserByEmail("jane.doe@example.com")).thenReturn(Optional.empty());
+
+        // Act
+        userService.updateUser(1L, "Jane", "Doe", "jane.doe@example.com");
+
+        // Assert
+        verify(userRepository).save(user);
+        assertEquals("Jane", user.getName());
+        assertEquals("Doe", user.getSurname());
+        assertEquals("jane.doe@example.com", user.getEmail());
     }
 
-    @Test
-    void updateUser() {
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateUserWithUsedEmail() {
+        // Setup
+        User user = new User(1L, "john.doe@example.com", "John", "Doe", LocalDate.of(1990, 1, 1));
+        User user2 = new User(2L, "jane.doe@example.com", "Jane", "Doe", LocalDate.of(1990, 1, 1));
+        when(userRepository.existsById(2L)).thenReturn(true);
+        when(userRepository.findById(2L)).thenReturn(Optional.of(user2));
+        when(userRepository.findUserByEmail("john.doe@example.com")).thenReturn(Optional.of(user));
+
+        // Act
+        userService.updateUser(2l,"Jane", "Doe", "john.doe@example.com");
+
+        // Assert
     }
 }
